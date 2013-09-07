8c7dab00e1b293cc03e0c7d64b07386979b55daf
Rohan Mathur <rohanmathur34@gmail.com>
2013-09-07 04:59:43 -0400

    Fix path for me
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
+++ obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -1,2 +1,145 @@
 obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
- jni/DetectionBasedTracker_jni.cpp
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/jni.h \
+ ../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../sdk/native/jni/include/opencv2/core/types_c.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/assert.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/string.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/strings.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/memory.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/math.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/limits.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ ../sdk/native/jni/include/opencv2/core/version.hpp \
+ /home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../sdk/native/jni/include/opencv2/core/operations.hpp \
+ /home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ ../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ ../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ /home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/jni.h:
+
+../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/string.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/math.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+../sdk/native/jni/include/opencv2/core/version.hpp:
+
+/home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/home/rohan/Desktop/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+/home/rohan/Desktop/ndk/platforms/android-8/arch-arm/usr/include/android/log.h:
