bf96be3b734cdba845b51678cb9acc730a62b4bb
Joe Doyle (Ginto8) <ginto8@gmail.com>
2013-09-07 22:29:03 -0400

    Got JNI shit working

--- jni/MatAverager_jni.cpp
+++ jni/MatAverager_jni.cpp
@@ -4,11 +4,11 @@
  *  Created on: Sep 7, 2013
  *      Author: Joe Doyle (Ginto8)
  */
-#include <MatAverager_jni.h>
+#include "MatAverager_jni.h"
 #include <opencv2/core/core.hpp>
 using namespace cv;
 
-JNIEXPORT void JNICALL Java_on_puz_photobombsquad_MatAverager_average(
+JNIEXPORT void JNICALL Java_org_on_puz_photobombsquad_MatAverager_average(
    JNIEnv * jenv, jclass, jlong matOut, jobjectarray mats) {
 	Mat& out = *((Mat*)matOut);
 	int n = GetArrayLength(jenv,mats);
---
+++ jni/MatAverager_jni.h
@@ -0,0 +1,15 @@
+#include <jni.h>
+
+#ifndef _Included_org_on_puz_photobombsquad_MatAverager
+#define _Included_org_on_puz_photobombsquad_MatAverager
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL Java_org_on_puz_photobombsquad_MatAverager_average(
+   JNIEnv * jenv, jclass, jlong matOut, jobjectArray mats);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
